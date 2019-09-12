859a87f17d58baacc892103fa4761bf57973114d
Joshua-Shockley <thelumbashockley@gmail.com>
2019-09-12 13:44:58 -0500

    getting started. setup with dependencies

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Yo.... <h3>This is My App!!!</h3></h1>
+    </div>
+  );
+}
+
+export default App;
